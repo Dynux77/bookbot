5afebee3ef36b674d96b5e04c1a5697a1d288b82
Dynux77 <dynux73@gmail.com>
2025-11-05 23:23:02 +0000

    finished the program

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 
+
+import sys
+
 from stats import word_counter
 from stats import get_book_text
+from stats import letter_counter
+from stats import list_of_values
+from stats import list_of_letters
+from stats import letter_num_pairing
+from stats import sorted_dic
+
+if len(sys.argv) < 2:
+   print("Usage: python3 main.py <path_to_book>")
+   sys.exit(1)
+else: 
+   file_path = sys.argv[1]
+
+   
 
 def main():
-   num_words = word_counter(get_book_text("books/frankenstein.txt"))
+   num_words = word_counter(get_book_text(file_path))
+   letter_count = letter_counter(get_book_text(file_path))
+   sorted_letters_nums = sorted_dic(letter_count)
+   letter_list = list_of_letters(sorted_letters_nums)
+   sorted_count = list_of_values(sorted_letters_nums)
+
+
+   print("============ BOOKBOT ============")
+   print(f"Analyzing book found at {file_path}...")
+   print("----------- Word Count ----------")
    print(f"Found {num_words} total words")
+   print("--------- Character Count -------")
+   letter_num_pairing(letter_list, sorted_count)
+
+   print(sys.argv)
+
+   
+
+   
+
+   
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+
+alphabet = "abcdefghijklmnopqrstuwxyzáäàæéèëóòöúùüãõñêâôû"
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -9,3 +13,36 @@ def word_counter(text):
     words = text.split()
     word_count = len(words)
     return word_count
+
+def letter_counter(words):
+    formatted_words = words.lower()
+    letter_dict = {}
+
+    for letter in alphabet:
+        counter = 0
+        for i in formatted_words:
+            if i == letter:
+                counter = counter + 1            
+            
+            else:
+                continue
+
+        letter_dict[letter] = counter
+    
+    return letter_dict
+
+def sorted_dic(dic):
+    return dict(sorted(dic.items(), key=lambda item: item[1], reverse=True))
+
+def list_of_values(dic):
+    return list(dic.values())
+
+def list_of_letters(dic):
+    return list(dic.keys())
+
+def letter_num_pairing(keys, values):
+    for i in range(len(keys)):
+        print(f"{keys[i]}: {values[i]}")
+
+
+
